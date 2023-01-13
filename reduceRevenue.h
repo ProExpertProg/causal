3a4046155db0166ebf03f20fa1319df3db73d19d
Luka Govedic <luka.govedic@gmail.com>
2023-01-13 18:25:15 -0500

    Extracted reduction into `reduce`

--- reduceRevenue.h
+++ reduceRevenue.h
@@ -19,6 +19,10 @@ template<Policy p>
 __always_inline void reduceRevenue(const Tensor<float> &revenue, const Tensor<float> &cost, Tensor<float> &profit,
                                    size_t N, size_t M, size_t geos, size_t teams);
 
+template<typename T>
+__always_inline T
+reduce(const Tensor<T> &revenue, size_t M, size_t i);
+
 template<>
 __always_inline void
 reduceRevenue<Policy::SERIAL>(const Tensor<float> &revenue, const Tensor<float> &cost, Tensor<float> &profit,
@@ -48,35 +52,41 @@ reduceRevenue<Policy::SERIAL_FAST>(const Tensor<float> &revenue, const Tensor<fl
 }
 
 template<>
+__always_inline float
+reduce<float>(const Tensor<float> &revenue, size_t M, size_t i) {
+    const Tensor<float> &revenue1 = revenue;// not proud of these magic constants here
+    constexpr size_t nPackedFloats = 16, // 512/32
+    maskTail = 15ul, // remainder after the loop
+    maskVec = ~maskTail;
+
+    __m512 sum = _mm512_set1_ps(0.0f);
+
+    for (size_t cohort = 0; cohort < (M & maskVec); cohort += nPackedFloats) {
+        // profit[i] += revenue[i * M + cohort];
+        // but do N_PACKED_FLOATS iterations in parallel
+        __m512 rev = _mm512_loadu_ps(&revenue1[i * M + cohort]);
+        sum = _mm512_add_ps(sum, rev);
+    }
+
+    // tail, add remaining elements
+    size_t remainder = M % nPackedFloats;
+    if (remainder) {
+        uint16_t m1 = 0xFFFFu >> (nPackedFloats - remainder);
+        auto mask = _load_mask16(&m1);
+        __m512 rev = _mm512_loadu_ps(&revenue1[i * M + (M & maskVec)]);
+        sum = _mm512_mask_add_ps(sum, mask, sum, rev);
+    }
+
+    // sum is in vector form,
+    return _mm512_reduce_add_ps(sum);
+}
+
+template<>
 __always_inline void
 reduceRevenue<Policy::SERIAL_VEC>(const Tensor<float> &revenue, const Tensor<float> &cost, Tensor<float> &profit,
                                   size_t N, size_t M, size_t geos, size_t teams) {
     for (size_t i = 0; i < geos * teams * N; ++i) {
-        // not proud of these magic constants here
-        constexpr size_t N_PACKED_FLOATS = 16, // 512/32
-        MASK_TAIL = 15ul, // remainder after the loop
-        MASK_VEC = ~MASK_TAIL;
-
-        __m512 sum = _mm512_set1_ps(0.0f);
-
-        for (size_t cohort = 0; cohort < (M & MASK_VEC); cohort += N_PACKED_FLOATS) {
-            // profit[i] += revenue[i * M + cohort];
-            // but do N_PACKED_FLOATS iterations in parallel
-            __m512 rev = _mm512_loadu_ps(&revenue[i * M + cohort]);
-            sum = _mm512_add_ps(sum, rev);
-        }
-
-        // tail, add remaining elements
-        size_t remainder = M % N_PACKED_FLOATS;
-        if (remainder) {
-            uint16_t m = 0xFFFFu >> (N_PACKED_FLOATS - remainder);
-            auto mask = _load_mask16(&m);
-            __m512 rev = _mm512_loadu_ps(&revenue[i * M + (M & MASK_VEC)]);
-            sum = _mm512_mask_add_ps(sum, mask, sum, rev);
-        }
-
-        // sum is in vector form,
-        profit[i] += _mm512_reduce_add_ps(sum) - cost[i];
+        profit[i] += reduce(revenue, M, i) - cost[i];
     }
 }
 
@@ -85,31 +95,8 @@ __always_inline void
 reduceRevenue<Policy::PARALLEL>(const Tensor<float> &revenue, const Tensor<float> &cost, Tensor<float> &profit,
                                 size_t N, size_t M, size_t geos, size_t teams) {
     cilk_for (size_t i = 0; i < geos * teams * N; ++i) {
-        // not proud of these magic constants here
-        constexpr size_t N_PACKED_FLOATS = 16, // 512/32
-        MASK_TAIL = 15ul, // remainder after the loop
-        MASK_VEC = ~MASK_TAIL;
-
-        __m512 sum = _mm512_set1_ps(0.0f);
-
-        for (size_t cohort = 0; cohort < (M & MASK_VEC); cohort += N_PACKED_FLOATS) {
-            // profit[i] += revenue[i * M + cohort];
-            // but do N_PACKED_FLOATS iterations in parallel
-            __m512 rev = _mm512_loadu_ps(&revenue[i * M + cohort]);
-            sum = _mm512_add_ps(sum, rev);
-        }
-
-        // tail, add remaining elements
-        size_t remainder = M % N_PACKED_FLOATS;
-        if (remainder) {
-            uint16_t m = 0xFFFFu >> (N_PACKED_FLOATS - remainder);
-            auto mask = _load_mask16(&m);
-            __m512 rev = _mm512_loadu_ps(&revenue[i * M + (M & MASK_VEC)]);
-            sum = _mm512_mask_add_ps(sum, mask, sum, rev);
-        }
-
         // sum is in vector form,
-        profit[i] += _mm512_reduce_add_ps(sum) - cost[i];
+        profit[i] += reduce(revenue, M, i) - cost[i];
     }
 }
 
@@ -126,4 +113,5 @@ reduceRevenue<Policy::PARALLEL_OMP>(const Tensor<float> &revenue, const Tensor<f
         profit[i] -= cost[i];
     }
 }
+
 #pragma clang diagnostic pop
